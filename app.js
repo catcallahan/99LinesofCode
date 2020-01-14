a94d10fc6bdc08fc525d7318d65f385d27d3cf7f
student <student@Admins-iMac.local>
2020-01-14 15:15:57 -0600

    got 100 lines of each friend

---
+++ app.js
@@ -0,0 +1,10 @@
+let friends = ['Avera', 'Brad', 'John', 'Mollie', 'Abby']
+function linesOfCode(friends) {
+    for(i=0;i< friends.length; i++){
+        for(a = 100; a > 0; a--) { 
+        console.log(friends[i])};
+    }
+}
+linesOfCode(friends);
+        
+
