52c9d4ffb925339e98e3db37de8ad1a29e083990
student <student@Admins-iMac.local>
2020-01-14 15:25:14 -0600

    both numbers descend

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let friends = ['Avera', 'Brad', 'John', 'Mollie', 'Abby']
 function linesOfCode(friends) {
     for(i=0;i< friends.length; i++){
         for(a = 100; a > 0; a--) { 
-        console.log(a + " lines of code in the file, " + a + " lines of code;" + friends[i] + " strikes one out, clears it all out," + a + " lines of code in the file")};
+        console.log(a + " lines of code in the file, " + a + " lines of code; " + friends[i] + " strikes one out, clears it all out," + [a = (a - 1)] + " lines of code in the file")};
     }
 }
 linesOfCode(friends);
